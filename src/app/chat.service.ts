326695949404865328b148ad1747dafdbda72a74
William Seto <wseto2@myseneca.ca>
2018-08-23 16:37:21 -0400

    can drop stuff now

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -3,6 +3,10 @@ import * as io from 'socket.io-client';
 import { Subject } from "rxjs";
 import { MatDialog, MatDialogConfig } from '@angular/material';
 import { DialogComponent } from './dialog/dialog.component';
+import { Router } from '@angular/router';
+import { Grid } from './models/grid';
+import { Column } from './models/column';
+import { Cell } from './models/cell';
 
 @Injectable({
   providedIn: 'root'
@@ -13,20 +17,24 @@ export class ChatService {
   public getMessages: any;
   public gameList: any[] = [];
   public userList: string[] = [];
+  public grid: Grid = new Grid;
+  currentGame: any;
   user: string;
+  private numRows: number = 6;
+  private numCols: number = 7;
+  public player: string = "red";
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, public router: Router) {
     this.getMessages = new Subject();
 
     this.socket = io.connect('https://mighty-spire-54148.herokuapp.com');
-
+    // this.socket = io.connect('localhost:8080');
     this.socket.on('chat message', (msg) => {
       this.getMessages.next(msg);
     });
 
     this.socket.on('add game', game => {
       this.gameList.push(game);
-      console.log(game);
     })
     this.socket.on('remove game', gameName => {
       for (let i = 0; i < this.gameList.length; i++) {
@@ -52,6 +60,52 @@ export class ChatService {
         this.userList.splice(index, 1);
       }
     })
+
+    this.socket.on('start game', () => {
+      this.grid.columns = [];
+      // Initialize grid model with empty cells
+      for (let i = 0; i < (this.numCols); i++) {
+        let column = new Column;
+        column.id = "column" + i;
+        column.cells = [];
+        column.count = 0;
+        for (let k = 0; k < (this.numRows); k++) {
+          let cell = new Cell;
+          cell.id = k;
+          cell.color = "none";
+          cell.state = 0;
+          column.cells.push(cell);
+        }
+        this.grid.columns.push(column);
+      }
+
+      for (let i = 0; i < this.gameList.length; i++) {
+        if (this.gameList[i].hostPlayer == this.socket.id || this.gameList[i].player == this.socket.id) {
+          this.currentGame = this.gameList[i];
+        }
+      }
+      this.router.navigate(['/game']);
+    })
+
+    this.socket.on('change state', (column, count) => {
+      this.grid.columns[column].cells[count - 1].state = 1;
+      this.grid.columns[column].cells[count - 1].color = this.player;
+      this.grid.columns[column].count++;
+      if (this.player == "red") {
+        this.player = "yellow";
+      } else {
+        this.player = "red";
+      }
+    })
+
+    this.socket.on('update game', (game) => {
+      for (let i = 0; i < this.gameList.length; i++) {
+        if (this.gameList[i].gameName == game.gameName) {
+          this.gameList[i] = game;
+          break;
+        }
+      }
+    })
   }
 
   sendMessage(msg) {
@@ -68,6 +122,22 @@ export class ChatService {
 
   removeGame(gameName) {
     this.socket.emit('remove game', gameName);
+  }
+
+  changeState(column) {
+    this.socket.emit('change state', column, this.currentGame, this.player);
+  }
+
+  connectHost(hostPlayer, status) {
+    if (this.socket.id != hostPlayer && status) {
+      for (let i = 0; i < this.gameList.length; i++) {
+        if (this.gameList[i].hostPlayer == hostPlayer) {
+          this.gameList[i].player = this.socket.id;
+          this.gameList[i].status = false;
+          this.socket.emit('join game', this.gameList[i]);
+        }
+      }
+    }
   }
 
   openDialog() {
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Grid } from '../models/grid';
 import { Column } from '../models/column';
 import { Cell } from '../models/cell';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-grid',
@@ -15,43 +16,44 @@ export class GridComponent implements OnInit {
   private numCols: number = 7;
   private player: string = "red";
 
-  constructor() { }
+  constructor(public chatService: ChatService) { }
 
   ngOnInit() {
-    this.grid.columns = [];
-    // Initialize grid model with empty cells
-    for (let i = 0; i < (this.numCols); i++) {
-      let column = new Column;
-      column.id = "column" + i;
-      column.cells = [];
-      column.count = 0;
-      for (let k = 0; k < (this.numRows); k++) {
-        let cell = new Cell;
-        cell.id = k;
-        cell.color = "none";
-        cell.state = 0;
-        column.cells.push(cell);
-      }
-      this.grid.columns.push(column);
-    }
+    // this.grid.columns = [];
+    // // Initialize grid model with empty cells
+    // for (let i = 0; i < (this.numCols); i++) {
+    //   let column = new Column;
+    //   column.id = "column" + i;
+    //   column.cells = [];
+    //   column.count = 0;
+    //   for (let k = 0; k < (this.numRows); k++) {
+    //     let cell = new Cell;
+    //     cell.id = k;
+    //     cell.color = "none";
+    //     cell.state = 0;
+    //     column.cells.push(cell);
+    //   }
+    //   this.grid.columns.push(column);
+    // }
   }
 
   changeState(column) {
-    if (column.count < 6) {
-      column.cells[column.count].state = 1;
-      column.cells[column.count].color = this.player;
-      column.count++;
-      if (this.player == "red") {
-        this.player = "yellow";
-      } else {
-        this.player = "red";
-      }
+    if (column < 6) {
+      this.chatService.changeState(column);
+      // column.cells[column.count].state = 1;
+      // column.cells[column.count].color = this.player;
+      // column.count++;
+      // if (this.player == "red") {
+      //   this.player = "yellow";
+      // } else {
+      //   this.player = "red";
+      // }
     }
   }
 
   overColor(column) {
     if (column.count < 6) {
-      column.cells[column.count].color = this.player;
+      column.cells[column.count].color = this.chatService.player;
     }
   }
 
